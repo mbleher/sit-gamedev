9cbb169daa83e6233676cf189bed99bcd8390ba2
Maxime Bleher <mbleher@stevens.edu>
2015-09-11 17:12:56 -0400

    Missing get/release implementation

--- src/engine/memory/counting_allocator.h
+++ src/engine/memory/counting_allocator.h
@@ -4,6 +4,7 @@
 # define INCLUDED_COUNTING_ALLOCATOR
 
 #include "default_allocator.h"
+#include <iostream>
 
 namespace StevensDev
 {
@@ -15,10 +16,8 @@ class CountingAllocator : public DefaultAllocator<T>
 private:
   int d_allocationCount;
   int d_releaseCount;
-  int d_outstandingCount;
   static int d_totalAllocationCount;
   static int d_totalReleaseCount;
-  static int d_totalOutstandingCount;
 public:
   // CONSTRUCTORS
   CountingAllocator();
@@ -29,13 +28,65 @@ public:
   //const CountingAllocator& operator=(const CountingAllocator&);
 
   // ACCESSORS
-  int const getAllocationCount();
-  int const getReleaseCount();
-  int const getOutstandingCount();
+  int getAllocationCount() const;
+  int getReleaseCount() const;
+  int getOutstandingCount() const;
+/*
   static int getTotalAllocationCount();
   static int getTotalReleaseCount();
   static int getTotalOutstandingCount();
+*/
 };
+
+template<typename T>
+inline
+CountingAllocator<T>::CountingAllocator() : d_allocationCount( 0 ), d_releaseCount( 0 )
+{
+}
+
+template<typename T>
+inline
+int CountingAllocator<T>::getAllocationCount() const
+{
+  return d_allocationCount;
+}
+
+template<typename T>
+inline
+int CountingAllocator<T>::getReleaseCount() const
+{
+  return d_releaseCount;
+}
+
+template<typename T>
+inline
+int CountingAllocator<T>::getOutstandingCount() const
+{
+  return d_allocationCount - d_releaseCount;
+}
+
+/*
+template<typename T>
+inline
+static int CountingAllocator<T>::getTotalAllocationCount()
+{
+  return d_totalAllocationCount;
+}
+
+template<typename T>
+inline
+static int CountingAllocator<T>::getReleaseCount()
+{
+  return d_totalReleaseCount;
+}
+
+template<typename T>
+inline
+static int CountingAllocator<T>::getTotalOutstandingCount()
+{
+  return d_totalAllocationCount - d_totalReleaseCount;
+}
+*/
 } // End sgdm namespace
 } // End StevensDev namespace
 
--- src/engine/memory/default_allocator.h
+++ src/engine/memory/default_allocator.h
@@ -13,11 +13,35 @@ template<typename T>
 class DefaultAllocator : public IAllocator<T>
 {
 public:
+  // CONSTRUCTORS
+  DefaultAllocator();
+
   T* get( int count );
     // FIXME
   void release( T*, int count );
     // FIXME
 };
+
+template<typename T>
+inline
+DefaultAllocator<T>::DefaultAllocator()
+{
+}
+
+template<typename T>
+inline
+T* DefaultAllocator<T>::get( int count )
+{
+  return 0;
+}
+
+template<typename T>
+inline
+void DefaultAllocator<T>::release( T*, int count )
+{
+  return;
+}
+
 } // End sgdm namespace
 } // End StevensDev namespace
 
