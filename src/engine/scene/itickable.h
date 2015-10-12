64a0577331b13fcd58ad4fcfa3ef1bd5878a6af2
Maxime Bleher <maxime.bleher@gmail.com>
2015-10-12 15:05:36 -0400

    ITickable implementation

---
+++ src/engine/scene/itickable.h
@@ -0,0 +1,20 @@
+// itickable.h
+
+#ifndef INCLUDED_ITICKABLE
+# define INCLUDED_ITICKABLE
+
+namespace StevensDev
+{
+namespace sgds
+{
+class ITickable
+{
+public:
+  virtual void preTick() = 0;
+  virtual void tick( float dtS ) = 0;
+  virtual void postTick() = 0;
+}; 
+} // End sgds namespace
+} // End StevensDev namespace
+
+#endif
