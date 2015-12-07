d0a2e8d8a8295dfa5f9858560ed6c08cbcd6ad50
Maxime BLEHER <maxime.bleher@gmail.com>
2015-12-07 16:25:39 -0500

    Cleaned main method

--- game.m.cpp
+++ game.m.cpp
@@ -9,31 +9,22 @@
 int main( int argc, char **argv )
 {
   using namespace StevensDev;
-  
-  sgdr::Renderer* renderer = new sgdr::Renderer();
-  renderer->loadTexture( "link", "../textures/link_mini.png" );
-    // 42x35
-  renderer->loadTexture( "map", "../textures/dungeonmap_big.png" );
-    // 512x446
-  renderer->setupWindow( MAPW, MAPH );
 
   sgds::Scene& scene = sgds::Scene::inst();
-  scene.setRenderer( renderer );
+  scene.setup();
 
-  // FIXME: I should probably not do all of this here
+  /*
   sgdr::RenderableSprite* link =
-    new sgdr::RenderableSprite( renderer->getTexture( "link" ) );
+    new sgdr::RenderableSprite( scene.renderer()->getTexture( "link" ) );
+
   mgc::PlayerController playerController;
   playerController.setSprite( link );
   playerController.sprite()->setPosition( MAPW / 2 - 17, MAPH - 60 ); 
-  renderer->addSprite(
-    new sgdr::RenderableSprite( renderer->getTexture( "map" ) ) );
-  renderer->addSprite( playerController.sprite() );
+  scene.renderer()->addSprite( playerController.sprite() );
   scene.addTickable( &playerController );
-
-  while( renderer->isActive() )
+  */
+  while( scene.renderer()->isActive() )
   {
-    renderer->draw();
     scene.tick();
   }
   
