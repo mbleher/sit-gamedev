61d1527380855cdcd56a384ae46968038764d10b
Maxime BLEHER <maxime.bleher@gmail.com>
2015-12-08 20:49:14 -0500

    Cleaned main

--- game.m.cpp
+++ game.m.cpp
@@ -1,10 +1,6 @@
 // game.m.cpp
 
 #include "scene.h"
-#include "player_controller.h"
-
-#define MAPW 512
-#define MAPH 446
 
 int main( int argc, char **argv )
 {
@@ -13,16 +9,6 @@ int main( int argc, char **argv )
   sgds::Scene& scene = sgds::Scene::inst();
   scene.setup();
 
-  /*
-  sgdr::RenderableSprite* link =
-    new sgdr::RenderableSprite( scene.renderer()->getTexture( "link" ) );
-
-  mgc::PlayerController playerController;
-  playerController.setSprite( link );
-  playerController.sprite()->setPosition( MAPW / 2 - 17, MAPH - 60 ); 
-  scene.renderer()->addSprite( playerController.sprite() );
-  scene.addTickable( &playerController );
-  */
   while( scene.renderer()->isActive() )
   {
     scene.tick();
