41a98b9c0cfc7318b5a839a32b5dcd77e7ace030
Maxime BLEHER <maxime.bleher@gmail.com>
2015-12-08 20:52:19 -0500

    Implemented MoveToController

--- src/game/controllers/move_to_controller.cpp
+++ src/game/controllers/move_to_controller.cpp
@@ -12,7 +12,16 @@ MoveToController::MoveToController()
 {
 }
 
+MoveToController::MoveToController( float x, float y )
+{
+  d_positions.push( new Position( x, y ) );
+  d_targetId = 0;
+}
+
 MoveToController::MoveToController( const MoveToController& copy )
+  : d_positions( copy.d_positions ),
+    d_targetId( copy.d_targetId ),
+    d_actor( copy.d_actor )
 {
 }
 
@@ -21,6 +30,56 @@ MoveToController::MoveToController( const MoveToController& copy )
 
 MoveToController::~MoveToController()
 {
+}
+
+void MoveToController::setActor( sgds::Actor* actor )
+{
+  d_actor = actor;
+}
+
+void MoveToController::addPosition( float x, float y )
+{
+  d_positions.push( new Position( x, y ) );
+}
+
+void MoveToController::preTick()
+{
+  return;
+}
+
+void MoveToController::tick( float dtS )
+{
+  if( d_actor->sprite()->getPositionX()
+      < d_positions[d_targetId]->first )
+  {
+    d_actor->move( 1, 0 );
+    return;
+  }
+  if( d_actor->sprite()->getPositionX()
+      > d_positions[d_targetId]->first )
+  {
+    d_actor->move( -1, 0 );
+    return;
+  }
+  if( d_actor->sprite()->getPositionY()
+      < d_positions[d_targetId]->second )
+  {
+    d_actor->move( 0, 1 );
+    return;
+  }
+  if( d_actor->sprite()->getPositionY()
+      > d_positions[d_targetId]->second )
+  {
+    d_actor->move( 0, -1 );
+    return;
+  }
+  d_targetId = ( d_targetId + 1 ) % d_positions.length();
+  return;
+}
+
+void MoveToController::postTick()
+{
+  return;
 }
 
 } // End mgc namespace
--- src/game/controllers/move_to_controller.h
+++ src/game/controllers/move_to_controller.h
@@ -2,22 +2,37 @@
 // Moves towards a position until it collides with something, then changes
 // direction.
 
-#include "renderable_sprite.h"
+#include "itickable.h"
+#include "actor.h"
+#include <utility>
 
 namespace StevensDev
 {
 namespace mgc
 {
-class MoveToController
+typedef std::pair<float, float> Position;
+class MoveToController : public sgds::ITickable
 {
   private:
+  sgdc::DynamicArray<Position*> d_positions;
+  unsigned short d_targetId;
+  sgds::Actor* d_actor;
   public:
   // CONSTRUCTORS
   MoveToController();
+  MoveToController( float x, float y );
     // Default constructor
   MoveToController( const MoveToController& copy );
     // Copy constructor
   ~MoveToController();
+
+  void setActor( sgds::Actor* actor );
+
+  void addPosition( float x, float y );
+
+  void preTick();
+  void tick( float dtS );
+  void postTick();
 };
 } // End mgc namespace
 } // End StevensDev namespace
